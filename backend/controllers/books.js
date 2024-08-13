976e90e63f4ce86744b84831e732db40564bd5c7
Neutron35 <steve.lieuron@gmail.com>
2024-08-13 10:16:08 +0200

    feat: created books and user controllers

---
+++ backend/controllers/books.js
@@ -0,0 +1,32 @@
+const Book = require('../models/Book');
+
+exports.getOneBook = (req, res, next) => {
+    Book.findOne({
+        _id: req.params._id
+    }).then(
+        (book) => {
+            res.status(200).json(book);
+        }
+    ).catch(
+        (error) => {
+            res.status(404).json({
+                error: error
+            });
+        }
+    );
+};
+
+exports.getAllBooks = (req, res, next) => {
+    Book.find().then(
+        (books) => {
+            res.status(200).json(books);
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
+}
+
