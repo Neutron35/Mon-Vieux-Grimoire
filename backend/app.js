f2e25cac25af792c6c170cf090ef57a8791d2c0a
Neutron35 <steve.lieuron@gmail.com>
2024-08-13 10:19:43 +0200

    feat: connected MongoDB to Express and hide some credentials

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const mongoCred = require('./cred');
+
+const userRoutes = require('./routes/user');
+const booksRoutes = require('./routes/books');
 
 const app = express();
+
+mongoose.connect(`mongodb+srv://${mongoCred.username}:${mongoCred.pwd}@${mongoCred.address}/?retryWrites=true&w=majority&appName=MVG`,
+    { useNewUrlParser: true,
+    useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -8,5 +21,8 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
+
+app.use('/api/auth', userRoutes);
+app.use('/api/books', booksRoutes);
 
 module.exports = app;
