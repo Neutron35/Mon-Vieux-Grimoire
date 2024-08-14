a07685b250d60f80f8218a14a7ae510ae52d0ed9
Neutron35 <steve.lieuron@gmail.com>
2024-08-14 11:06:57 +0200

    fix: small typo in a field's name

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -7,10 +7,12 @@ const bookSchema = mongoose.Schema({
     imageUrl: { type: String, required: true},
     year: { type: Number, required: true },
     genre: { type: String, required: true },
-    rating: {
+    ratings: [
+        {
             userId: { type: String, required: true },
             grade: { type: Number, required: true}
-    },
+        }
+    ],
     averageRating: { type: Number, required: true }
 });
 
