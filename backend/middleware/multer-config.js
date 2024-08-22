744fa7eaec5a95cfdfe5e6c655812c0c0bf9009b
Neutron35 <steve.lieuron@gmail.com>
2024-08-22 13:26:20 +0200

    feat: now using nanoid for images
    Multer middleware now uses nanoid to provide a unique ID to the image file name, preventing having twice the same file from two different users

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,4 +1,5 @@
 import multer from 'multer';
+import { nanoid } from 'nanoid';
 
 const MIME_TYPES = {
   'image/jpg': 'jpg',
@@ -20,18 +21,8 @@ const storage = multer.diskStorage({
       .join('.');
     const extension = MIME_TYPES[file.mimetype];
 
-    const date = new Date();
-    const formatWithLeadingZero = (number) => (number < 10 ? '0' : '') + number;
-    const year = date.getFullYear();
-    const month = formatWithLeadingZero(date.getMonth() + 1);
-    const day = formatWithLeadingZero(date.getDate());
-    const hours = formatWithLeadingZero(date.getHours());
-    const minutes = formatWithLeadingZero(date.getMinutes());
-    const seconds = formatWithLeadingZero(date.getSeconds());
-
-    const timestamp = `${year}${month}${day}-${hours}${minutes}${seconds}`;
-
-    callback(null, `${name}_${timestamp}.${extension}`);
+    const uniqueId = nanoid();
+    callback(null, `${name}_${uniqueId}.${extension}`);
   },
 });
 
