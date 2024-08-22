f2be0a5b37ca383a4d66290bee960b3c69b2b7df
Neutron35 <steve.lieuron@gmail.com>
2024-08-22 13:23:27 +0200

    feat: submitted image is now resized

--- backend/middleware/sharp-config.js
+++ backend/middleware/sharp-config.js
@@ -13,6 +13,10 @@ const optimizeImage = async (req, res, next) => {
 
   try {
     await sharp(originalImagePath)
+      .resize({
+        height: 570,
+        fit: 'contain',
+      })
       .toFormat('webp')
       .webp({ quality: 80 })
       .toFile(webpImagePath);
